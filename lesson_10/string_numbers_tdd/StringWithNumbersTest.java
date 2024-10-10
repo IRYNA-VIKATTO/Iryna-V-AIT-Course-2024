a60a9da593b1e9212a642421af25df4649cf5233
vicattoIryna <vicatto@ukr.net>
2024-10-10 10:53:24 +0200

    Classwork 9.10.2024

---
+++ lesson_10/string_numbers_tdd/StringWithNumbersTest.java
@@ -0,0 +1,30 @@
+package string_numbers_tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringWithNumbersTest {
+
+    StringWithNumbers stringWithNumbers;// object
+
+    @BeforeEach
+    void setUp() {
+        stringWithNumbers = new StringWithNumbers("123 321 10");//454
+
+
+
+    }
+
+    @Test
+    void sumOfNumbersInStringTest(){
+
+        String str = "123 321 10";
+        int expected = 454;
+        int actual = stringWithNumbers.sumOfNumbersInString(str);
+        assertEquals(expected, actual);
+        
+    }
+
+}
