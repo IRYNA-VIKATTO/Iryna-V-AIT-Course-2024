617e79a666173c4a9fe9ee4bc0052870aff27042
vicattoIryna <vicatto@ukr.net>
2024-11-21 00:37:02 +0100

    Exception - исключение

---
+++ lesson_50/code/classwork_50/exceptions/exception_handlir/NoSolutionException.java
@@ -0,0 +1,11 @@
+package classwork_50.exceptions.exception_handlir;
+
+public class NoSolutionException extends Exception{
+
+    public NoSolutionException() {
+    }
+
+    public NoSolutionException(String message){
+        super(message);
+    }
+}
---
+++ lesson_50/code/classwork_50/exceptions/exception_handlir/SolutionIsAnyNumber.java
@@ -0,0 +1,13 @@
+package classwork_50.exceptions.exception_handlir;
+
+public class SolutionIsAnyNumber extends RuntimeException{
+
+    // empty constructor
+    public SolutionIsAnyNumber() {
+    }
+
+    public SolutionIsAnyNumber(String message) {
+        super(message);
+    }
+
+}
